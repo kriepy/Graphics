52ebb24a37ea5499d5f52604a60bf5e183ee9a12
kriepy <k.rieping@gmail.com>
2012-06-26 17:16:56 +0200

    merge

--- GraphicsPractical3/GraphicsPractical3/GraphicsPractical3/Game1.cs
+++ GraphicsPractical3/GraphicsPractical3/GraphicsPractical3/Game1.cs
@@ -28,24 +28,17 @@ namespace GraphicsPractical3
         Effect effect2;
         Model model;
         Model model2;
+        //Model[] model2 = new Model[4];
+        Material modelMaterial;
 
-        // for rotation and translation
+        // Quad
+        VertexPositionNormalTexture[] quadVertices;
+        short[] quadIndices;
+        Matrix quadTransform;
+
+        //   for rotation
         float rotationAmount = 0;
-
-        // Post Proccesing
-        private Effect postEffect;
-
-        // Set render target
-        RenderTarget2D renderTarget;
-
-        // For switching excersizes
-        int ExcNum = 0;
-        int maxExc = 3;
-        bool postGray = false;
-        bool postBlur = false;
-        bool Bpressed = false;
-        bool Gpressed = false;
-        bool SpacePressed = false;
+        int ExcNum = 1;
 
         public Game1()
         {
@@ -60,7 +53,7 @@ namespace GraphicsPractical3
 
 
         protected override void Initialize()
-        {   
+        {
             device = graphics.GraphicsDevice;
             // Copy over the device's rasterizer state to change the current fillMode
             device.RasterizerState = new RasterizerState() { CullMode = CullMode.None };
@@ -75,11 +68,6 @@ namespace GraphicsPractical3
             graphics.ApplyChanges();
             // Initialize the camera
             camera = new Camera(camEye, new Vector3(0, 0, 0), new Vector3(0, 1, 0));
-
-            // initialize render target
-            renderTarget = new RenderTarget2D(device, device.PresentationParameters.BackBufferWidth,
-                device.PresentationParameters.BackBufferHeight, false, device.PresentationParameters.BackBufferFormat,
-                DepthFormat.Depth24);
 
             IsMouseVisible = true;
 
@@ -102,8 +90,7 @@ namespace GraphicsPractical3
             // Load the model and let it use the "Simple" effect
             model = Content.Load<Model>("Model/femalehead");
 
-            // Load the "PostProcessing" effect
-            postEffect = Content.Load<Effect>("Effect/postProccesing");
+            model2 = Content.Load<Model>("Model/femalehead");
 
             // Setup the quad
             //SetupQuad();
@@ -119,47 +106,23 @@ namespace GraphicsPractical3
         protected override void Update(GameTime gameTime)
         {
             model.Meshes[0].MeshParts[0].Effect = effect[ExcNum];
-
+            model2.Meshes[0].MeshParts[0].Effect = effect2;
             float timeStep = (float)gameTime.ElapsedGameTime.TotalSeconds * 60.0f;
 
             //Keyboard usage
             KeyboardState KeyState = Keyboard.GetState();
 
             //Change Rotation using  the 'left' and 'right' keys
-            if (KeyState.IsKeyDown(Keys.Z))
+            if (KeyState.IsKeyDown(Keys.Left))
             {
                 rotationAmount = rotationAmount + timeStep / 100;
             }
 
-            if (KeyState.IsKeyDown(Keys.G))
-            {
-                if (!Gpressed) {postGray = !postGray;}
-                Gpressed = true;
-            }
-            if (KeyState.IsKeyUp(Keys.G)) {Gpressed = false;}
-
-            if (KeyState.IsKeyDown(Keys.B))
-            {
-                if (!Bpressed) { postBlur = !postBlur; }
-                Bpressed = true;
-            }
-            if (KeyState.IsKeyUp(Keys.B)) { Bpressed = false; }
-            
-
-            if (KeyState.IsKeyDown(Keys.X))
+            if (KeyState.IsKeyDown(Keys.Right))
             {
                 rotationAmount = rotationAmount - timeStep / 100;
             }
 
-            if (KeyState.IsKeyDown(Keys.Space))
-            {
-                if (!SpacePressed) { ExcNum = (ExcNum + 1) % (maxExc+1); }
-                SpacePressed = true;
-            }
-            if (KeyState.IsKeyUp(Keys.Space)) {SpacePressed = false;}
-
-            // Update Excersize
-            model.Meshes[0].MeshParts[0].Effect = effect[ExcNum];
             // Update the window title
             Window.Title = "XNA Renderer | FPS: " + frameRateCounter.FrameRate;
 
@@ -167,12 +130,10 @@ namespace GraphicsPractical3
         }
 
 
+
+
         protected override void Draw(GameTime gameTime)
         {
-            // set render target
-            device.SetRenderTarget(renderTarget);
-            device.DepthStencilState = new DepthStencilState() { DepthBufferEnable = true };
-
             // Clear the screen in a predetermined color and clear the depth buffer
             device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.DeepSkyBlue, 1.0f, 0);
             Matrix World = Matrix.CreateScale(2.0f);
@@ -182,9 +143,7 @@ namespace GraphicsPractical3
             // Get the model's only mesh
             ModelMesh mesh = model.Meshes[0];
             Effect effect = mesh.Effects[0];
-
-            Vector3 Light = new Vector3(0,0, 40);
-
+            Vector3 Light = new Vector3(0, 0, 40);
             switch (ExcNum)
             {
                 case 0:
@@ -224,9 +183,9 @@ namespace GraphicsPractical3
                     lightColors[1] = Color.Blue.ToVector4();
                     lightColors[2] = Color.Green.ToVector4();
                     Vector3[] lightPositions = new Vector3[3];
-                    lightPositions[0] = new Vector3(-20, 0, 40);
-                    lightPositions[1] = new Vector3(10, -20, 80);
-                    lightPositions[2] = new Vector3(10, 40, 80);
+                    lightPositions[0] = new Vector3(-10, -20, 50);
+                    lightPositions[1] = new Vector3(10, -20, 50);
+                    lightPositions[2] = new Vector3(0, 40, -80);
                     effect.Parameters["LightPositions"].SetValue(lightPositions);
                     effect.Parameters["LightColors"].SetValue(lightColors);
                     effect.Parameters["World"].SetValue(World);
@@ -270,27 +229,8 @@ namespace GraphicsPractical3
 
                 // do nothing for now
             }
-
-            //Draw the model
-            mesh.Draw();
-
-            //Handle Post Effects
-            if (postGray) { postEffect.Parameters["ApplyGray"].SetValue(true); }
-            else { postEffect.Parameters["ApplyGray"].SetValue(false); }
-            if (postBlur) { 
-                postEffect.Parameters["ApplyBlur"].SetValue(true);
-            }
-            else { postEffect.Parameters["ApplyBlur"].SetValue(false); }
-
-            device.SetRenderTarget(null);
-            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Opaque,
-                    SamplerState.LinearClamp, DepthStencilState.Default,
-                    RasterizerState.CullNone, postEffect);
-            spriteBatch.Draw(renderTarget, new Rectangle(0, 0, 800, 600), Color.White);
-            spriteBatch.End();
-
             // Draw the model
-            //mesh.Draw();
+            mesh.Draw();
 
             base.Draw(gameTime);
         }
